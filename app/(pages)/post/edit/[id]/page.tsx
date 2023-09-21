294fa47640e7727bffef9feae6e79ba8fc03139d
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-09-21 09:38:49 +0300

    dynamic edit url has been added

---
+++ app/(pages)/post/edit/[id]/page.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const fetchPost = async (id: string) => {
+    try {
+        const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
+        return data;
+    } catch (_) {
+        return null;
+    }
+};
+
+export default async function Page({
+                                       params: { id },
+                                   }: {
+    params: { id: string };
+}) {
+
+    const post = await fetchPost(id);
+
+
+    return (<div className="grid gap-3 content-start">
+        {post ? (
+            <>
+                <h1 className="text-4xl">{post.title}</h1>
+                <p className="text-lg">{post.body}</p>
+            </>
+        ) : (
+            <h1 className="text-4xl">Post Not Found</h1>
+        )}
+    </div>);
+}
